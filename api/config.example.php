f59bef582e9b6a622ead587a59776a1fe0a3325a
jhunaeycodex <junaidijheva@gmail.com>
2026-06-14 11:00:32 +0700

    Add MySQL config example

---
+++ api/config.example.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'host' => '127.0.0.1',
+    'port' => '3306',
+    'database' => 'bbfs7d',
+    'username' => 'bbfs7d_user',
+    'password' => 'GANTI_PASSWORD_MYSQL',
+    'charset' => 'utf8mb4',
+];
